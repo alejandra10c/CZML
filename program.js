2de2fc54fdfeb3003ac3c0cceb3199f7e3b44182
alejandra10c <alejandrachingateacc@gmail.com>
2025-08-16 08:46:48 -0500

    colombia2

--- program.js
+++ program.js
@@ -1,12 +1,12 @@
 async function init() {
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyYzZjNjkwNy1hNmJjLTQ3Y2EtOWQ5NS0yYzM4Mzk0YzkzNjciLCJpZCI6MzE4NTIsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTYwNjUyNzB9.RvMzC5gf0JKfZzhl3nX0g60_MqWICPLGfshdhNhscH4';
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNzI1ZjBlNS1hNmY4LTQ0OGEtYjE5OS1hMGJiYjYzMmNhOGIiLCJpZCI6MzMxMzc2LCJpYXQiOjE3NTUwNDI2Mjd9.vMkh7Pvq2F9MhZE4H7wqPFFQs1gPKdc-Ax4q0tncRs8';
 
     const viewer = new Cesium.Viewer('cesiumContainer', {
         terrain: Cesium.Terrain.fromWorldTerrain(),
     });
 
     try {
-        const ds = await Cesium.CzmlDataSource.load('colombia_formas.czml');
+        const ds = await Cesium.CzmlDataSource.load('escena.czml');
         viewer.dataSources.add(ds);
         await viewer.zoomTo(ds);
     } catch (e) {
